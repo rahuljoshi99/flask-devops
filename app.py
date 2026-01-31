af0c9aac77ffea90f2ba52dafddd03d5aba43c1c
Ubuntu <ubuntu@ip-172-31-46-27.ap-south-1.compute.internal>
2026-01-31 06:42:12 +0000

    Initial DevOps project

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import os
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Flask + Docker + Jenkins 🚀"
+
+@app.route("/health")
+def health():
+    return "OK", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
